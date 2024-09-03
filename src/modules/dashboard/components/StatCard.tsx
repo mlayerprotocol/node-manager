8fe776029907fda6d10942b862ea0caa987e43f7
Abdulbassit Abdullahi <funabab@gmail.com>
2024-09-03 02:12:18 +0100

    feat: integrate dashboard contract

--- src/modules/dashboard/components/StatCard.tsx
+++ src/modules/dashboard/components/StatCard.tsx
@@ -7,18 +7,31 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export type StatCardProps = {
   title: string;
   value: string;
+  loading?: boolean;
   meta?: {
     title: string;
-    value: string;
-  };
+  } & (
+    | {
+        requiresConnection: boolean;
+      }
+    | { value: string }
+  );
   icon: React.ReactNode;
 };
 
-export default function StatCard({ title, value, meta, icon }: StatCardProps) {
+export default function StatCard({
+  title,
+  value,
+  meta,
+  icon,
+  loading,
+}: StatCardProps) {
   return (
     <Card className="border border-[#28303F] rounded-lg bg-[#101020]">
       <CardHeader>
@@ -26,15 +39,31 @@ export default function StatCard({ title, value, meta, icon }: StatCardProps) {
           {title}
         </CardTitle>
         <CardContent className="p-0 mt-2 pb-7">
-          <p className="font-bold text-2xl text-white">{value}</p>
+          {loading ? (
+            <Skeleton className="h-8 w-60" />
+          ) : (
+            <p className="font-bold text-2xl text-white">{value}</p>
+          )}
         </CardContent>
         <CardFooter className="p-0 flex items-center justify-between">
           {meta && (
-            <div>
+            <div className="flex flex-col gap-1">
               <strong className="text-sm text-[#AEB9E1] font-bold">
                 {meta.title}
               </strong>
-              <p className="text-sm text-white font-bold">{meta.value}</p>
+              {loading ? (
+                <Skeleton className="h-5 w-28" />
+              ) : (
+                <>
+                  {"value" in meta ? (
+                    <p className="text-sm text-white font-bold">{meta.value}</p>
+                  ) : (
+                    meta.requiresConnection && (
+                      <Badge variant="outline">Connect wallet to fetch</Badge>
+                    )
+                  )}
+                </>
+              )}
             </div>
           )}
           <Button className="bg-gradient-texture w-10 h-10 p-0 ml-auto" asChild>
--- src/modules/dashboard/containers/DashboardContainer.tsx
+++ src/modules/dashboard/containers/DashboardContainer.tsx
@@ -13,38 +13,20 @@ import {
   ChartTooltipContent,
 } from "@/components/ui/chart";
 import { Label, Pie, PieChart } from "recharts";
+import { useAccount } from "wagmi";
+import {
+  useReadChainInfoContractGetCurrentCycle,
+  useReadSubnetContractAddressInfo,
+} from "@mlayer-contracts";
+import { Badge } from "@/components/ui/badge";
+import { Skeleton } from "@/components/ui/skeleton";
+import { ImSpinner } from "react-icons/im";
 
-const STATS: StatCardProps[] = [
-  {
-    title: "xMSG Earned",
-    value: "23,2300",
-    meta: {
-      title: "Previous Cycle",
-      value: "320023",
-    },
-    icon: <HiArrowUturnLeft size={20} />,
-  },
-  {
-    title: "xMSG Accrued",
-    value: "3,2300",
-    meta: {
-      title: "Current Cycle",
-      value: "320024",
-    },
-    icon: <FaRecycle size={20} />,
-  },
-  {
-    title: "xMSG Claimable",
-    value: "3,2300",
-    icon: <FaWallet size={20} />,
-  },
-];
-
-const chartData = [
-  { status: "delegated", percentage: 100, fill: "var(--color-delegated)" },
-  { status: "pending", percentage: 0, fill: "var(--color-pending)" },
-  { status: "free", percentage: 0, fill: "var(--color-free)" },
-];
+// const chartData = [
+//   { status: "delegated", percentage: 100, fill: "var(--color-delegated)" },
+//   { status: "pending", percentage: 0, fill: "var(--color-pending)" },
+//   // { status: "free", percentage: 0, fill: "var(--color-free)" },
+// ];
 
 const chartConfig = {
   delegated: {
@@ -55,13 +37,61 @@ const chartConfig = {
     label: "Pending",
     color: "#2F5ED2",
   },
-  free: {
-    label: "Free",
-    color: "#5812C9",
-  },
+  // free: {
+  //   label: "Free",
+  //   color: "#5812C9",
+  // },
 } satisfies ChartConfig;
 
+function getPercentage(value: number, total: number) {
+  if (total === 0) {
+    return 0;
+  }
+  return (value / total) * 100;
+}
+
 export default function DashboardContainer() {
+  const { address, isConnected } = useAccount();
+  const { data: getAddressInfo, isLoading: getAddressInfoIsLoading } =
+    useReadSubnetContractAddressInfo({
+      args: address && [address],
+      query: {
+        enabled: isConnected,
+      },
+    });
+  const { data: getCurrentCycle, isLoading: getCurrentCycleIsLoading } =
+    useReadChainInfoContractGetCurrentCycle({
+      query: {
+        enabled: isConnected,
+      },
+    });
+
+  const licenses = useMemo(() => {
+    if (!getAddressInfo) {
+      return {
+        total: 0,
+        pending: 0,
+        delegated: 0,
+      };
+    }
+    return getAddressInfo[2].reduce<{ total: 0; pending: 0; delegated: 0 }>(
+      (acc, license) => {
+        acc.total += 1;
+        if (Boolean(license.delegatedTo) && license.delegatedTo !== "0x") {
+          acc.delegated += 1;
+        } else {
+          acc.pending += 1;
+        }
+        return acc;
+      },
+      {
+        total: 0,
+        pending: 0,
+        delegated: 0,
+      }
+    );
+  }, [getAddressInfo]);
+
   const legend = useMemo(() => {
     return (
       <ul className="list-none space-y-5">
@@ -73,27 +103,69 @@ export default function DashboardContainer() {
             ></span>
             <span className="text-white text-sm">{config.label}</span>
             <span className="text-white text-sm">
-              ({chartData.find((data) => data.status === key)?.percentage}
+              (
+              {key === "delegated"
+                ? getPercentage(licenses.delegated, licenses.total)
+                : getPercentage(licenses.pending, licenses.total)}
               %)
             </span>
           </li>
         ))}
       </ul>
     );
-  }, []);
+  }, [licenses]);
 
   return (
     <main className="px-5 py-10 md:px-20 md:py-20 container mx-auto">
       <div className="grid lg:grid-cols-2 xl:grid-cols-3 gap-4 lg:gap-8">
-        {STATS.map((stat, index) => (
-          <StatCard key={index} {...stat} />
-        ))}
+        <StatCard
+          title="xMSG Earned"
+          value={`${getAddressInfo?.[0] || 0}`}
+          meta={{
+            title: "Previous Cycle",
+            ...(isConnected
+              ? {
+                  value: `${getCurrentCycle ? `${Number(getCurrentCycle) - 1}` : 1}`,
+                }
+              : { requiresConnection: true }),
+          }}
+          icon={<HiArrowUturnLeft size={20} />}
+          loading={getAddressInfoIsLoading || getCurrentCycleIsLoading}
+        />
+
+        <StatCard
+          title="xMSG Accrued"
+          value={`${getAddressInfo?.[1] || 0}`}
+          meta={{
+            title: "Current Cycle",
+            ...(isConnected
+              ? {
+                  value: `${getCurrentCycle ? `${Number(getCurrentCycle)}` : 1}`,
+                }
+              : { requiresConnection: true }),
+          }}
+          icon={<FaRecycle size={20} />}
+          loading={getAddressInfoIsLoading || getCurrentCycleIsLoading}
+        />
+
+        <StatCard
+          title="xMSG Claimable"
+          value={`${getAddressInfo?.[3] || 0}`}
+          icon={<FaWallet size={20} />}
+          loading={getAddressInfoIsLoading}
+        />
       </div>
 
       <div className="py-20 flex flex-col gap-10 lg:gap-14 lg:flex-row items-center justify-center">
         <Card className="py-4 px-8 w-full lg:w-max">
           <CardHeader className="p-0">
-            <CardTitle>30 of 30 Licenses Delegated</CardTitle>
+            <CardTitle>
+              {getAddressInfoIsLoading ? (
+                <Skeleton className="w-60 h-6" />
+              ) : (
+                `${licenses.delegated} of ${licenses.total} Licenses Delegated`
+              )}
+            </CardTitle>
           </CardHeader>
           <CardContent className="p-0 mt-7">
             <div className="flex items-center justify-between gap-10 lg:gap-20">
@@ -114,51 +186,84 @@ export default function DashboardContainer() {
               </Button>
             </div>
             <div className="mt-7">
-              <ChartContainer
-                config={chartConfig}
-                className="mx-auto aspect-square max-h-[250px]"
-              >
-                <PieChart>
-                  <ChartTooltip
-                    cursor={false}
-                    content={<ChartTooltipContent valuePostfix="%" hideLabel />}
-                  />
-                  <Pie
-                    data={chartData}
-                    dataKey="percentage"
-                    nameKey="status"
-                    innerRadius={60}
-                    strokeWidth={5}
-                  >
-                    <Label
-                      content={({ viewBox }) => {
-                        if (viewBox && "cx" in viewBox && "cy" in viewBox) {
-                          return (
-                            <text
-                              x={viewBox.cx}
-                              y={viewBox.cy}
-                              textAnchor="middle"
-                              dominantBaseline="middle"
-                            >
-                              <tspan
+              {licenses.total > 0 ? (
+                <ChartContainer
+                  config={chartConfig}
+                  className="mx-auto aspect-square max-h-[250px]"
+                >
+                  <PieChart>
+                    <ChartTooltip
+                      cursor={false}
+                      content={
+                        <ChartTooltipContent valuePostfix="%" hideLabel />
+                      }
+                    />
+                    <Pie
+                      data={[
+                        {
+                          status: "delegated",
+                          percentage: getPercentage(
+                            licenses.delegated,
+                            licenses.total
+                          ),
+                        },
+                        {
+                          status: "pending",
+                          percentage: getPercentage(
+                            licenses.pending,
+                            licenses.total
+                          ),
+                        },
+                      ]}
+                      dataKey="percentage"
+                      nameKey="status"
+                      innerRadius={60}
+                      strokeWidth={5}
+                    >
+                      <Label
+                        content={({ viewBox }) => {
+                          if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                            return (
+                              <text
                                 x={viewBox.cx}
                                 y={viewBox.cy}
-                                className="fill-foreground text-2xl font-bold"
+                                textAnchor="middle"
+                                dominantBaseline="middle"
                               >
-                                100%
-                              </tspan>
-                            </text>
-                          );
-                        }
-                      }}
-                    />
-                  </Pie>
-                </PieChart>
-              </ChartContainer>
+                                <tspan
+                                  x={viewBox.cx}
+                                  y={viewBox.cy}
+                                  className="fill-foreground text-2xl font-bold"
+                                >
+                                  100%
+                                </tspan>
+                              </text>
+                            );
+                          }
+                        }}
+                      />
+                    </Pie>
+                  </PieChart>
+                </ChartContainer>
+              ) : (
+                <div className="w-full aspect-square flex justify-center items-center max-h-[250px]">
+                  {getAddressInfoIsLoading ? (
+                    <ImSpinner className="text-5xl animate-spin duration-1000" />
+                  ) : (
+                    <Badge variant="outline">
+                      {isConnected
+                        ? "No License purchased"
+                        : "Connect wallet to fetch chart"}
+                    </Badge>
+                  )}
+                </div>
+              )}
             </div>
           </CardContent>
         </Card>
-        <div className="lg:my-auto lg:mb-9">{legend}</div>
+        {!getAddressInfoIsLoading && (
+          <div className="lg:my-auto lg:mb-9">{legend}</div>
+        )}
       </div>
     </main>
   );
